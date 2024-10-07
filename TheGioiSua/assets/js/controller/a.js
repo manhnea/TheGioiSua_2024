1068ad0556364b62dd6adab9969ebba0524be96c
Hieu <Hieudzpro4444@gmail.com>
2024-10-07 19:52:14 +0700

    login

--- TheGioiSua/assets/js/controller/a.js
+++ TheGioiSua/assets/js/controller/a.js
@@ -16,3 +16,42 @@ if (token) {
 } else {
     console.error("No token found. User might not be logged in.");
 }
+// assets/js/controller/MainController.js
+angular.module('myApp')
+    .controller('MainController', ['$scope', '$location', function ($scope, $location) {
+        $scope.isLoginPage = false;
+
+        // Theo dõi sự thay đổi URL
+        $scope.$on('$routeChangeSuccess', function () {
+            // Kiểm tra xem đường dẫn hiện tại có phải là trang đăng nhập không
+            if ($location.path() === '/login') {
+                $scope.isLoginPage = true; // Ẩn header và footer
+            } else {
+                $scope.isLoginPage = false; // Hiển thị header và footer
+            }
+        });
+    }]);
+// assets/js/app.js
+angular.module('myApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'assets/views/home.html',
+                controller: 'HomeController'
+            })
+            .when('/products', {
+                templateUrl: 'assets/views/products.html',
+                controller: 'ProductController'
+            })
+            .when('/contact', {
+                templateUrl: 'assets/views/contact.html',
+                controller: 'ContactController'
+            })
+            .when('/login', {
+                templateUrl: 'assets/views/login.html', // Đường dẫn tới tệp HTML cho trang đăng nhập
+                controller: 'LoginController'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
+    }]);
