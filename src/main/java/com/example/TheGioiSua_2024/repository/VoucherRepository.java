f72854827c40898a14b5e42a701cd9ccdec07444
NguyenTruong01 <truongnguyen010123@gmail.com>
2024-10-15 16:14:58 +0700

    done

--- src/main/java/com/example/TheGioiSua_2024/repository/VoucherRepository.java
+++ src/main/java/com/example/TheGioiSua_2024/repository/VoucherRepository.java
@@ -1,9 +1,15 @@
 package com.example.TheGioiSua_2024.repository;
 
+
 import com.example.TheGioiSua_2024.entity.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface VoucherRepository extends JpaRepository<Voucher, Long> {
+    @Query("select a from Voucher a WHERE a.vouchercode = ?1")
+    Optional<Voucher> findByVoucher(String voucherName);
 }
--- src/main/java/com/example/TheGioiSua_2024/service/impl/IVoucherService.java
+++ src/main/java/com/example/TheGioiSua_2024/service/impl/IVoucherService.java
@@ -1,12 +1,15 @@
 package com.example.TheGioiSua_2024.service.impl;
 
+import com.example.TheGioiSua_2024.entity.Milktaste;
 import com.example.TheGioiSua_2024.entity.Voucher;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IVoucherService {
     List<Voucher> getVoucherList();
-    Voucher saveVoucher(Voucher voucher);
-    Voucher updateVoucher(Long id, Voucher voucher);
+    String saveVoucher(Voucher voucher);
+    String updateVoucher(Long id, Voucher voucher);
     Voucher deleteVoucher(Long id);
+    Optional<Voucher> getVoucherByName(String voucherName);
 }
