932afc5f13f7dce177c364d27320c9897c1b8370
basil204 <96483932+basil204@users.noreply.github.com>
2024-10-18 16:15:32 +0700

    update

--- src/main/java/com/example/TheGioiSua_2024/service/MilktypeService.java
+++ src/main/java/com/example/TheGioiSua_2024/service/MilktypeService.java
@@ -14,12 +14,10 @@ import java.util.Optional;
 public class MilktypeService implements IMilktypeService {
     @Autowired
     MilktypeRepository milktypeRepository;
-
     @Override
     public String AddMilktype(MilkType milktype) {
         String trimmedName = milktype.getMilkTypename().trim();
         milktype.setMilkTypename(trimmedName);
-        // Kiểm tra xem tên  đã tồn tại chưa
         Optional<MilkType> existingContainer = getMilkTypeByName(trimmedName);
         if (existingContainer.isPresent()) {
             return "Tên này đã tồn tại.";
@@ -28,7 +26,6 @@ public class MilktypeService implements IMilktypeService {
         milktypeRepository.save(milktype);
         return "Them Thanh Cong";
     }
-
     @Override
     public String UpdateMilktype(Long id, MilkType milktype) {
         MilkType existingMilkType = milktypeRepository.findById(id).orElseThrow();
@@ -37,9 +34,9 @@ public class MilktypeService implements IMilktypeService {
             existingMilkType.setDescription(milktype.getDescription());
             existingMilkType.setStatus(1);
             milktypeRepository.save(existingMilkType);
-            return "Cập nhật mô tả thương hiệu sữa thành công.";
+            return "Cập nhật mô tả loại sữa sữa thành công.";
         } else if (milktypeRepository.findByMilkTypename(milktype.getMilkTypename()).isPresent()) {
-            return "Thương hiệu sữa này đã tồn tại.";
+            return "loại sữa sữa này đã tồn tại.";
         }
         MilkType milktype1 = milktypeRepository.findById(id).orElseThrow();
         milktype1.setDescription(milktype.getDescription());
@@ -47,7 +44,6 @@ public class MilktypeService implements IMilktypeService {
          milktypeRepository.save(milktype1);
          return "Sua Thanh Cong";
     }
-
     @Override
     public MilkType DeleteMilktype(Long id) {
         MilkType milktype1 = milktypeRepository.findById(id).orElseThrow();
@@ -59,13 +55,10 @@ public class MilktypeService implements IMilktypeService {
     public Optional<MilkType> getMilkTypeByName(String milktypename) {
         return milktypeRepository.findByMilkTypename(milktypename);
     }
-
     @Override
     public MilkType GetMilktypeById(Long id) {
         return milktypeRepository.findById(id).orElseThrow();
     }
-
-
     @Override
     public List<MilkType> GetAllMilktype() {
         return milktypeRepository.findAll();
