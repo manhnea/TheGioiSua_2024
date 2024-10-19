f9af886a8552593dc14997e1703aa3b6a2264959
basil204 <96483932+basil204@users.noreply.github.com>
2024-10-19 09:44:13 +0700

    update lai validate đăng ký

--- src/main/java/com/example/TheGioiSua_2024/dto/RegisterDto.java
+++ src/main/java/com/example/TheGioiSua_2024/dto/RegisterDto.java
@@ -14,7 +14,6 @@ import java.io.Serializable;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegisterDto implements Serializable {
-    //it's a Data Trasfer Object for registration
     String username ;
     String password ;
     String fullname ;
