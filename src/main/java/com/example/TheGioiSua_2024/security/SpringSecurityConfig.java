daef7d7f11c3fcf78c36197670f9d851f99dd5a5
Hieu <Hieudzpro4444@gmail.com>
2024-10-26 21:48:47 +0700

    update_MilkDetail

--- src/main/java/com/example/TheGioiSua_2024/security/SpringSecurityConfig.java
+++ src/main/java/com/example/TheGioiSua_2024/security/SpringSecurityConfig.java
@@ -17,7 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,56 +27,72 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SpringSecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter ;
-    private final CustomerUserDetailsService customerUserDetailsService ;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomerUserDetailsService customerUserDetailsService;
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception
-    { http
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests()
-            .requestMatchers(
-                    "/user/**",
-                    "/Product/page",
-                    "/Product/page/**",
-                    "/Userinvoice/lst",
-                    "/Packagingunit/lst",
-                    "/Usagecapacity/lst",
-                    "/Milkdetail/page",
-                    "/Milkdetail/getMilkDetail",
-                    "/Milktype/lst",
-                    "/Milkbrand/lst",
-                    "/Targetuser/lst",
-                    "/Milktaste/lst",
-                    "/Invoice/lst").permitAll()
-            .requestMatchers(
-                    "/admin/**",
-                    "/Voucher/**",
-                    "/Milktype/**",
-                    "/Milktaste/**",
-                    "/Packagingunit/**",
-                    "/Targetuser/**",
-                    "/Product/**",
-                    "/Milkdetail/**",
-                    "/Milkbrand/**",
-                    "/Invoicedetail/**",
-                    "/Invoice/**",
-                    "/Userinvoice/**",
-                    "/Usagecapacity/**").hasAuthority("Admin");
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors() // Kết hợp cấu hình CORS với Spring Security
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/user/**",
+                        "/Product/page",
+                        "/Product/page/**",
+                        "/Userinvoice/lst",
+                        "/Packagingunit/lst",
+                        "/Usagecapacity/lst",
+                        "/Milkdetail/page",
+                        "/Milkdetail/getMilkDetail",
+                        "/Milktype/lst",
+                        "/Milkbrand/lst",
+                        "/Targetuser/lst",
+                        "/Milktaste/lst",
+                        "/Invoice/lst").permitAll()
+                .requestMatchers(
+                        "/admin/**",
+                        "/Voucher/**",
+                        "/Milktype/**",
+                        "/Milktaste/**",
+                        "/Packagingunit/**",
+                        "/Targetuser/**",
+                        "/Product/**",
+                        "/Milkdetail/**",
+                        "/Milkbrand/**",
+                        "/Invoicedetail/**",
+                        "/Invoice/**",
+                        "/Userinvoice/**",
+                        "/Usagecapacity/**").hasAuthority("Admin");
 //            .requestMatchers("/user/**").hasAuthority("Customer") ;
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return  http.build();
+        return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
-    { return authenticationConfiguration.getAuthenticationManager();}
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder()
-    { return new BCryptPasswordEncoder(); }
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:5500");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
